2f88e0619440689a76cfc6ffd4817acbef22ca89
SamSumit007 <97454993+SamSumit007@users.noreply.github.com>
2022-02-21 17:39:20 +0530

    Create sidebar.js

---
+++ Template-U3C4/components/sidebar.js
@@ -0,0 +1,20 @@
+function sidebar() {
+
+    return `
+
+    <div>Menu</div>
+    <div>Login</div>
+    <div>Sign up</div>
+    <input type="text" id="searchbar" />
+    <div>News</div>
+
+
+    <div>API</div>
+    
+    `
+
+
+    // return your html component here
+    //Make sure to give input search box id as ""
+}
+export default sidebar
